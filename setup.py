4e31825386db92ea50149f762a72d590b672e3e7
Darya Vozhdaeva <darya.vozhdaeva@ocrv.ru>
2021-01-12 15:37:14 +0300

    Delete torchaudio from requirements and add read, getversion functions

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
+import os
 
-with open("README.md", "r") as readme_file:
-    readme = readme_file.read()
+
+def read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        file_content = f.read()
+    return file_content
 
 def get_version():
     """Get version from the package without actually importing it."""
@@ -10,7 +14,7 @@ def get_version():
         if line.startswith('__version__'):
             return eval(line.split('=')[1])
 
-requirements = ["librosa>=0.6.1,<=0.8.0", "colorednoise>=1.1.1", "torchaudio>=0.7.2"]
+requirements = ["librosa>=0.6.1,<=0.8.0", "colorednoise>=1.1.1"]
 
 setup(
     name="speechaugs",
@@ -18,7 +22,7 @@ setup(
     author="Darya Vozhdaeva",
     author_email="daria-vozhdaeva@yandex.ru",
     description="Waveform augmentations",
-    long_description=readme,
+    long_description=read('README.txt'),
     long_description_content_type="text/markdown",
     url="https://github.com/waveletdeboshir/speechaugs/",
     packages=find_packages(),
