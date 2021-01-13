49d8e3e3e3ca6133bf646448acff62cd85d77865
Darya Vozhdaeva <darya.vozhdaeva@ocrv.ru>
2021-01-13 17:49:58 +0300

    Change version

--- speechaugs/__init__.py
+++ speechaugs/__init__.py
@@ -1,5 +1,5 @@
 from .speechaugs import BaseWaveformTransform, TimeStretchLibrosa, PitchShiftLibrosa, WhiteNoise, PinkNoise, BrownNoise, ColoredNoise, ForwardTimeShift, Inversion, ZeroSamples, ClippingSamples
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 
 __all__ = ['BaseWaveformTransform', 'TimeStretchLibrosa', 'PitchShiftLibrosa', 'WhiteNoise', 'PinkNoise', 'BrownNoise', 'ColoredNoise', 'ForwardTimeShift', 'Inversion', 'ZeroSamples', 'ClippingSamples']
