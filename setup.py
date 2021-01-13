b3f2ad7e1876785021e8a35e7da6cf97e6f3252e
Darya Vozhdaeva <darya.vozhdaeva@ocrv.ru>
2021-01-13 15:53:56 +0300

    Add lib

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+
+def read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        file_content = f.read()
+    return file_content
+
+def get_version():
+    """Get version from the package without actually importing it."""
+    init = read('speechaugs/__init__.py')
+    for line in init.split('\n'):
+        if line.startswith('__version__'):
+            return eval(line.split('=')[1])
+
+requirements = ["librosa>=0.6.1,<=0.8.0", "colorednoise>=1.1.1", "albumentations>=0.5.2"]
+
+setup(
+    name="speechaugs",
+    version=get_version(),
+    author="Darya Vozhdaeva",
+    author_email="daria-vozhdaeva@yandex.ru",
+    description="Waveform augmentations",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    url="https://github.com/waveletdeboshir/speechaugs/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
